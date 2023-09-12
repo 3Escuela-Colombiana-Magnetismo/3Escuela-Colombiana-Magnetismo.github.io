9329f79f47265104ae94c0f728afdba8636964b8
fquevedo1 <fquevedo@unal.edu.co>
2023-09-12 11:43:36 -0500

    cambios en la versión movil

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// JavaScript puro
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    var menu = document.querySelector('.menu');
-    if (menu.style.display === 'none' || menu.style.display === '') {
-      menu.style.display = 'block';
-    } else {
-      menu.style.display = 'none';
-    }
-  });
-  
-  // Con jQuery
-  $('.menu-icon').click(function() {
-    $('.menu').toggle();
-  });
-  
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  var menu = document.querySelector(".menu");
+  menu.classList.icon("show");
+});
