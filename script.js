6d5a06d1d3049bd86f9ae9a743cd52fd218a9d2f
fquevedo1 <fquevedo@unal.edu.co>
2023-09-12 12:31:51 -0500

    cambios en el menu de la versión movil

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector(".menu-icon").addEventListener("click", function () {
   var menu = document.querySelector(".menu");
-  menu.classList.icon("show");
+  menu.classList.toggle("show");
 });
